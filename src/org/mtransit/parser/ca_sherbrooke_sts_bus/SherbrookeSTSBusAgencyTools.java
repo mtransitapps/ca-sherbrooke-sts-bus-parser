de134820c9d7de3f40f3647a88f069ab1de60ff3
Mathieu Méa <mathieu.mea@gmail.com>
2018-07-17 11:08:11 -0400

    Compatible with Current/Next GTFS data

--- src/org/mtransit/parser/ca_sherbrooke_sts_bus/SherbrookeSTSBusAgencyTools.java
+++ src/org/mtransit/parser/ca_sherbrooke_sts_bus/SherbrookeSTSBusAgencyTools.java
@@ -46,6 +46,11 @@ public class SherbrookeSTSBusAgencyTools extends DefaultAgencyTools {
 	}
 
 	@Override
+	public boolean excludingAll() {
+		return this.serviceIds != null && this.serviceIds.isEmpty();
+	}
+
+	@Override
 	public boolean excludeCalendar(GCalendar gCalendar) {
 		if (this.serviceIds != null) {
 			return excludeUselessCalendar(gCalendar, this.serviceIds);
